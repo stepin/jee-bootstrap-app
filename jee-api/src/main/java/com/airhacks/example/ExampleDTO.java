89247895d562592c63875dca09f4bd66df8786ee
Igor Stepin <stepin@stepinsoft.com>
2017-03-04 12:23:17 +0400

    fixes, added lombok configuration and examples

---
+++ jee-api/src/main/java/com/airhacks/example/ExampleDTO.java
@@ -0,0 +1,10 @@
+package com.airhacks.example;
+
+import lombok.Data;
+
+@Data
+public class ExampleDTO {
+    private String name;
+    private String description;
+    private int amount;
+}
---
+++ jee-api/src/main/java/com/airhacks/example/ExampleService.java
@@ -0,0 +1,13 @@
+package com.airhacks.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExampleService {
+
+    public void example(){
+        ExampleDTO exampleDTO = new ExampleDTO();
+        exampleDTO.setName("name");
+        log.info(exampleDTO.toString());
+    }
+}
